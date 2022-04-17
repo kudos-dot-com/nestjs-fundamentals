abbe925116e519aae13ab3d7b69b1d91702d138c
bhaskar sengupta <sajal1943@gmail.com>
2022-04-17 19:01:05 +0530

    #10 implemented notfound exception

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import {InjectModel} from '@nestjs/mongoose';
+import { throws } from 'assert/strict';
 import { Model } from 'mongoose';
 import { CreateUserDto } from './dto/create-users.dto';
 import { UserSchema } from './schema/user.schema';
@@ -12,11 +13,25 @@ export class UsersService {
         return await this.UserModel.find();
     }
     async findOne(email:string): Promise<CreateUserDto>{
-      return await this.UserModel.findOne({email});
+      const user = await this.UserModel.findOne({email});
+      if(user)
+      {
+          return user
+      }
+      else{
+          throw new NotFoundException("User not found");
+      }
     }
     async findOneById(id:string): Promise<CreateUserDto>{
-        return await this.UserModel.findById({_id:id});
-      }
+        const user = await this.UserModel.findById({_id:id});
+        if(user)
+        {
+            return user
+        }
+        else{
+            throw new NotFoundException("User not found");
+        }
+    }
     async addUser(user:CreateUserDto): Promise<CreateUserDto>{
         const newUser = new this.UserModel(user);
         return await newUser.save();
