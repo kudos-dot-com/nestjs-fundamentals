0d43e064c6e13b025227db455dc77a10a22d00c9
bhaskar sengupta <sajal1943@gmail.com>
2022-04-17 18:49:59 +0530

    #7 added auth guard in user services

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,21 +1,26 @@
-import { Controller,Post,Body,Get, Param, BadRequestException, ArgumentMetadata, PipeTransform } from '@nestjs/common';
+import { Controller,Post,Body,Get, Param, BadRequestException, ArgumentMetadata, PipeTransform, UseGuards } from '@nestjs/common';
 import { get } from 'http';
 import { CreateUserDto } from './dto/create-users.dto';
 import { UsersService } from './users.service';
 import {checkId , ValidationPipe} from './pipes/validation.pipes';
-
+import { LocalAuthGuard } from '../auth/local-auth.guard';
 
 @Controller('users')
 export class UsersController {
     constructor(private readonly usersService: UsersService) {}
+    @UseGuards(LocalAuthGuard)
     @Get()
     async findAll(): Promise<CreateUserDto[]> {
         return await this.usersService.findAll();
     }
+    
+    @UseGuards(LocalAuthGuard)
     @Get(':id')
     async findOne(@Param('id',new checkId()) id): Promise<CreateUserDto> {
         return await this.usersService.findOne(id);
     }
+
+    @UseGuards(LocalAuthGuard)
     @Post('/create')
     async create(@Body(new ValidationPipe()) createuser : CreateUserDto): Promise<CreateUserDto> {
         return await this.usersService.addUser(createuser);
