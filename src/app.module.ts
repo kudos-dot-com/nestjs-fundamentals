270b6e1069bd13f05e5a8a9930d2618ec23e1bc1
bhaskar sengupta <sajal1943@gmail.com>
2022-04-18 14:39:19 +0530

    #13 removed payment folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,8 @@ import { UsersController } from './users/users.controller';
 import { UserModule } from './users/users.module'
 import {MongooseModule} from '@nestjs/mongoose'
 import { UsersService } from './users/users.service';
-import { PaymentsController } from './payments/payments.controller';
-import { PaymentsService } from './payments/payments.service';
 import config from './config/keys';
 import { UserSchema } from './users/schema/user.schema';
-import { PaymentModule } from './payments/payments.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 
@@ -20,10 +17,9 @@ import { ConfigModule } from '@nestjs/config';
     MongooseModule.forRootAsync({useFactory:()=>({uri:process.env.MONGO_URI})}),
     UserModule,
     AuthModule,
-    PaymentModule.forRoot(config.stripeKey,{apiVersion:'2020-08-27'}),
     AuthModule],
     
-  controllers: [AppController, UsersController, PaymentsController],
-  providers: [AppService, UsersService, PaymentsService],
+  controllers: [AppController, UsersController],
+  providers: [AppService, UsersService],
 })
 export class AppModule {}
