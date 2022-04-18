41a70e13234aa7a40d0bcc9332d66ff1bdd882de
bhaskar sengupta <sajal1943@gmail.com>
2022-04-18 14:40:07 +0530

    #14 removed config folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,6 @@ import { UsersController } from './users/users.controller';
 import { UserModule } from './users/users.module'
 import {MongooseModule} from '@nestjs/mongoose'
 import { UsersService } from './users/users.service';
-import config from './config/keys';
 import { UserSchema } from './users/schema/user.schema';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
